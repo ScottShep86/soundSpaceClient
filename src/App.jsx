58288d66546a3f3565f28a77fb03d8e15425b712
Scott Shepherd <scottshep86@gmail.com>
2024-02-27 12:50:31 +0100

    feature: Homepage, Login and Register pages setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import LogIn from './pages/LogIn';
+import Register from './pages/Register';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LogIn />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom';
+
 export default function HomePage() {
   return (
     <div>
       <h1>SoundSpace</h1>
+      <Link to={'/login'}>LOGIN</Link>
+      <Link to={'/register'}>REGISTER</Link>
     </div>
   );
 }
