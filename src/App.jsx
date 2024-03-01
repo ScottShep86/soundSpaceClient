cc0c0186070995dc8753c753a2fe3ecb00809a45
Scott Shepherd <scottshep86@gmail.com>
2024-03-01 11:13:28 +0100

    feature: set up and use of IsAnon and IsPrivate components

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,37 @@ import HomePage from './pages/HomePage';
 import LogIn from './pages/auth/LogIn';
 import Register from './pages/auth/Register';
 import Profile from './pages/user/Profile';
+import IsAnon from './components/IsAnon';
+import IsPrivate from './components/IsPrivate';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LogIn />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/profile" element={<Profile />} />
+      <Route
+        path="/login"
+        element={
+          <IsAnon>
+            <LogIn />
+          </IsAnon>
+        }
+      />
+      <Route
+        path="/register"
+        element={
+          <IsAnon>
+            <Register />
+          </IsAnon>
+        }
+      />
+      <Route
+        path="/profile"
+        element={
+          <IsPrivate>
+            <Profile />
+          </IsPrivate>
+        }
+      />
     </Routes>
   );
 }
